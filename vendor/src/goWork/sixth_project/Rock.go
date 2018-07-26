126b99cf8efb5ac9c1f2d05b5ad90d6996fe9f8a
david_li <david_li@mail.chungyo.net>
2018-07-26 14:57:20 +0800

    [修改]govendor

---
+++ vendor/src/goWork/sixth_project/Rock.go
@@ -0,0 +1,19 @@
+package Rocket
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Rocket interface {
+	Name() string  //火箭名稱
+	PushTime() int //倒數時程
+}
+
+func Launch(rocket Rocket) {
+	fmt.Println(rocket.Name() + " 開始倒數發射,倒數 " + strconv.Itoa(rocket.PushTime()) + " 秒")
+	for i := rocket.PushTime(); i >= 0; i-- {
+		fmt.Printf("%d\n", i)
+	}
+	fmt.Printf("%s 已升空 \n", rocket.Name())
+}
